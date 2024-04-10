ffa1fd38f7ff8db1b1c306ccff22e8c49abaa48e
sotol067 <luz.soto.-nd@disney.com>
2024-04-10 15:54:54 -0500

    Deleted imports of components that are not necesary to be imported on app.component.ts file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HomeComponent } from './pages/home/home.component';
-import { LoginComponent } from './pages/login/login.component';
-import { ProfileComponent } from './pages/profile/profile.component';
-import { RegisterComponent } from './pages/register/register.component';
 import { NavBarComponent } from './shared/components/molecules/nav-bar/nav-bar.component';
-
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [ 
-    HomeComponent,
-    LoginComponent, 
-    ProfileComponent, 
-    RegisterComponent, 
     RouterOutlet, 
     NavBarComponent
   ],
