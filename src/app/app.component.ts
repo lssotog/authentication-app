292eb855d5bc9ebb92bc908616e16ba239896203
lssotog <68559589+lssotog@users.noreply.github.com>
2024-04-10 17:28:05 -0500

    Apply suggestions from code review
    Co-authored-by: Harley Santos Garzón <harley.santos.garzon@gmail.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 import { ProfileComponent } from './pages/profile/profile.component';
